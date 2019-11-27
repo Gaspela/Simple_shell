1d73d09c31615fbe1c2003835d28c8e1f9b6087c
Gaspela04 <samirmillanorozco@hotmail.com>
2019-11-27 20:17:19 +0000

    String copy

---
+++ _strcmp.c
@@ -0,0 +1,20 @@
+#include "shell.h"
+/**
+ * _strcmp - Funtion compare two string.
+ * @s1: String one.
+ * @s2: String two.
+ * Return: zero is equal.
+*/
+int _strcmp(const char *s1,const char *s2)
+{
+	int i;
+/* Print string */
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+/* Compare string */
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+
+	return (0);
+}
