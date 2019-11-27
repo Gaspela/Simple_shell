7d3ff5140667613666c43bf00d3d32720d2117d9
Gaspela04 <samirmillanorozco@hotmail.com>
2019-11-27 19:35:28 +0000

    Print's

---
+++ _printf.c
@@ -0,0 +1,70 @@
+#include "shell.h"
+/**
+ * _printf - print the variables
+ * @string: constant pointer chat type.
+ * Return: const.
+ */
+int _printf(const char *string, ...)
+{
+	char *data;
+	int i;
+	int strlen;
+	int cont = 0;
+	va_list val;
+
+	if (string == NULL)
+	return (-1);
+	va_start(val, string);
+	for (i = 0; string[i]; i++)
+	{
+		if (string[i] == '%')
+		{
+			i++;
+			switch (string[i])
+			{
+				case 'c':
+				cont += _putchar(va_arg(val, int));
+					break;
+					case 's':
+					data = va_arg(val, char *);
+					if (data == NULL)
+					{
+						data = "(NULL)";
+						_puts(data);
+						cont += _strlen(data);
+						break;
+					}
+					strlen = _strlen(data);
+					if (data[strlen] != 0)
+						return (-1);
+						_puts(data);
+						cont += _strlen(data);
+						break;
+					case '%':
+						cont += _putchar(string[i]);
+						break;
+					case 'i':
+						cont += display_number(va_arg(val, int));
+						break;
+					case 'd':
+						cont += display_number(va_arg(val, int));
+						break;
+					case ' ':
+						return (-1);
+					case '\0':
+						return (-1);
+					default:
+						_putchar('%');
+						_putchar(string[i]);
+							cont += 2;
+							break;
+					}
+			}
+			else
+		{
+		cont += _putchar(string[i]);
+		}
+	}
+	va_end(val);
+	return (cont);
+}
